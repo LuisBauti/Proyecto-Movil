2e194146149d6ab09f43d43709c75031c2fd287e
Luis Flores <lf016158@gmail.com>
2021-12-02 23:29:16 -0600

    se termino del minuto 11 al minuto 22

--- proyectofinal/lib/shoes.dart
+++ proyectofinal/lib/shoes.dart
@@ -1,31 +1,29 @@
-
-
-class NikeShoes{
-  final String ? model;
-  final double ? oldPrice;
-  final double ? currentPrice;
-  final List<String> ? images;
-  final int ? modelNumber;
-  final int ? color;
-  NikeShoes({
-    this.model,
-    this.oldPrice,
-    this.currentPrice,
-    this.images,
-    this.modelNumber,
-    this.color
-  });
-  
+class NikeShoes {
+  final String model;
+  final double oldPrice;
+  final double currentPrice;
+  final List<String>? images;
+  final int? modelNumber;
+  final int? color;
+  NikeShoes(
+      {required this.model,
+      required this.oldPrice,
+      required this.currentPrice,
+      this.images,
+      this.modelNumber,
+      this.color});
 }
 
-final shoes=<NikeShoes>[
+final shoes = <NikeShoes>[
   NikeShoes(
     model: 'AIR MAX 90 EZ BLACK',
     currentPrice: 150,
     oldPrice: 300,
-    images: [ 'assets/image1.1.png',
-              'assets/image1.2.png',
-              'assets/image1.3.png'],
+    images: [
+      'assets/image1.1.png',
+      'assets/image1.2.png',
+      'assets/image1.3.png'
+    ],
     modelNumber: 90,
     color: 0xFFF6F6F6,
   ),
@@ -33,9 +31,11 @@ final shoes=<NikeShoes>[
     model: 'Air Max 270 GOLD',
     currentPrice: 160,
     oldPrice: 320,
-    images: [ 'assets/image2.1.png',
-              'assets/image2.2.png',
-              'assets/image2.3.png'],
+    images: [
+      'assets/image2.1.png',
+      'assets/image2.2.png',
+      'assets/image2.3.png'
+    ],
     modelNumber: 270,
     color: 0xfffef8e3,
   ),
@@ -43,9 +43,11 @@ final shoes=<NikeShoes>[
     model: 'Air Max 270 RED',
     currentPrice: 90,
     oldPrice: 150,
-    images: [ 'assets/image3.1.png',
-              'assets/image3.2.png',
-              'assets/image3.3.png'],
+    images: [
+      'assets/image3.1.png',
+      'assets/image3.2.png',
+      'assets/image3.3.png'
+    ],
     modelNumber: 95,
     color: 0xfffee8e3,
   ),
@@ -53,9 +55,11 @@ final shoes=<NikeShoes>[
     model: 'Team Hustle Quick 98',
     currentPrice: 205.97,
     oldPrice: 300,
-    images: [ 'assets/image4.1.png',
-              'assets/image4.2.png',
-              'assets/image4.3.png'],
+    images: [
+      'assets/image4.1.png',
+      'assets/image4.2.png',
+      'assets/image4.3.png'
+    ],
     modelNumber: 98,
     color: 0xffe3f3fe,
   ),
--- proyectofinal/lib/shoes_details.dart
+++ proyectofinal/lib/shoes_details.dart
@@ -1,52 +1,232 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:proyectofinal/shoes.dart';
 
 class NikeShoesDetails extends StatelessWidget {
+  final NikeShoes shoes;
+  final ValueNotifier<bool> notifierButtonsVisible = ValueNotifier(false);
 
-  final ValueNotifier<bool> notifierButtonsVisible=ValueNotifier(false);
+  NikeShoesDetails({Key? key, required this.shoes}) : super(key: key);
+  Widget _buildCarousel(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.5,
+      child: Stack(
+        children: <Widget>[
+          Positioned.fill(
+              child: Hero(
+            tag: 'background_${shoes.model}',
+            child: Container(
+              color: Color(0xFBF5EB),
+            ),
+          )),
+          Positioned(
+              left: 70,
+              right: 70,
+              top: 30,
+              child: Hero(
+                  tag: 'number_${shoes.model}',
+                  child: Material(
+                    color: Colors.transparent,
+                    child: FittedBox(
+                      child: Text(
+                        shoes.modelNumber.toString(),
+                      ),
+                    ),
+                  ))),
+          PageView.builder(
+            itemCount: shoes.images?.length,
+            itemBuilder: (context, index) {
+              final tag = index == 0
+                  ? 'image_${shoes.model}'
+                  : 'image_${shoes.model}_$index';
+              return Container(
+                  alignment: Alignment.center,
+                  child: Hero(
+                    tag: tag,
+                    child: Image.asset(
+                      shoes.images![index],
+                      height: 200,
+                      width: 200,
+                    ),
+                  ));
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  get child => null;
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance!.addPostFrameCallback((_){
-      notifierButtonsVisible.value=true;
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      notifierButtonsVisible.value = true;
     });
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Image.asset(
+          'assets/logo.png',
+          height: 40,
+        ),
+        leading: BackButton(
+          color: Colors.black,
+        ),
+      ),
       body: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
+          Positioned.fill(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildCarousel(context),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              shoes.model,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const Spacer(),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    '\$${shoes.oldPrice.toInt().toString()}',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      decoration: TextDecoration.lineThrough,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$${shoes.currentPrice.toInt().toString()}',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Available sizes",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            _ShoeSizeItem(
+                              text: "6",
+                            ),
+                            _ShoeSizeItem(
+                              text: "7",
+                            ),
+                            _ShoeSizeItem(
+                              text: "9",
+                            ),
+                            _ShoeSizeItem(
+                              text: "10",
+                            ),
+                            _ShoeSizeItem(
+                              text: "11",
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Descripción",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        )
+                      ]),
+                )
+              ],
+            ),
+          ),
           ValueListenableBuilder<bool>(
-            valueListenable: notifierButtonsVisible,
-            child:  Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: <Widget>[
-                      FloatingActionButton(
+              valueListenable: notifierButtonsVisible,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: <Widget>[
+                    FloatingActionButton(
                         heroTag: "btn1",
                         backgroundColor: Colors.white,
-                        child: Icon(Icons.favorite, color: Colors.black,),
-                        onPressed: (){}),
-                      Spacer(),
-                      FloatingActionButton(
+                        child: Icon(
+                          Icons.favorite,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {}),
+                    Spacer(),
+                    FloatingActionButton(
                         heroTag: "btn2",
                         backgroundColor: Colors.black,
                         child: Icon(Icons.shopping_cart),
-                        onPressed: (){}
-                      ),
-                      
-                      
-                    ],
-                  ),
+                        onPressed: () {}),
+                  ],
                 ),
-            builder: (context, value, child) {
-              return AnimatedPositioned(
-                duration: const Duration(milliseconds:200 ),
-                left: 0,
-                right: 0,
-                bottom: value ? 0.0 : -kToolbarHeight,
-                child: child!,
-              );
-            }
-          )
-        ],),
+              ),
+              builder: (context, value, child) {
+                return AnimatedPositioned(
+                  duration: const Duration(milliseconds: 200),
+                  left: 0,
+                  right: 0,
+                  bottom: value ? 0.0 : -kToolbarHeight,
+                  child: child!,
+                );
+              })
+        ],
+      ),
+    );
+  }
+}
+
+class _ShoeSizeItem extends StatelessWidget {
+  final String? text;
+
+  const _ShoeSizeItem({Key? key, this.text}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Text(
+        'US ${text}',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 11,
+        ),
+      ),
     );
   }
 }
