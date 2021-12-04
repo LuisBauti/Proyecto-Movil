584153d2ddd07c866b9406165fc413b9b5f4d7dd
Luis Flores <lf016158@gmail.com>
2021-12-04 16:19:36 -0600

    falta de terminar ultima animacion

--- proyectofinal/lib/shoes_details.dart
+++ proyectofinal/lib/shoes_details.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+//@Dart=2.9
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:proyectofinal/shoes.dart';
